2f00257de95392c5375e045c52184100cf136131
kevinsmgov <kevin.hobson@smgov.net>
2018-05-26 11:44:01 -0700

    client side xml processing

---
+++ asp_classic/client-sql-xml.asp
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<% 
+    If Request.QueryString("xml").Count = 1 then
+        Set DatabaseConnection = Server.CreateObject("ADODB.Connection") 
+        Set DatabaseCommand = Server.CreateObject("ADODB.Command") 
+        Set DatabaseStream = Server.CreateObject("ADODB.Stream")
+        Set customerOrdersXML = Server.CreateObject( "Microsoft.XMLDOM" )
+        DatabaseConnection.Open Application("ConnectionStringSQL")
+        DatabaseStream.Open
+        if DatabaseConnection.State = adStateOpen then
+            DatabaseCommand.ActiveConnection = DatabaseConnection
+            DatabaseCommand.CommandText = "SELECT Customers.CustomerID, Customers.ContactName, Orders.OrderID, Orders.OrderDate, [Order Details].UnitPrice, [Order Details].Quantity, [Order Details].Discount, Products.ProductName FROM Customers INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID INNER JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID INNER JOIN Products ON [Order Details].ProductID = Products.ProductID ORDER BY Customers.ContactName, Orders.OrderDate FOR XML AUTO, ROOT('root')"
+            DatabaseCommand.Properties("Output Stream") = DatabaseStream
+            DatabaseCommand.Execute , , 1024 'instead of adExecuteStream
+            DatabaseStream.Position = 0
+            if customerOrdersXML.LoadXML(DatabaseStream.ReadText) then
+                Response.Clear
+                Response.ContentType = "text/xml"
+                customerOrdersXML.Save(Response)
+                Response.End
+            End if
+        End if
+    End if
+%>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Classic ASP - Database</title>
+    <style>
+        body {
+            width: 66em;
+        }
+    </style>
+</head>
+<body>
+    <h1>Classic ASP - Database</h1>
+    <!-- #include file="menu.asp" -->
+    <p>AJAX request</p>
+    <ul id="customers">
+    </ul>
+</body>
+</html>
+<script src="client-sql-xml.js" type="text/javascript">
+</script>
---
+++ asp_classic/client-sql-xml.js
@@ -0,0 +1,49 @@
+﻿
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+    xhttp.open("GET", "client-sql-xml.asp?xml", true);
+    xhttp.send();
+}
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    var customers = xmlDoc.getElementsByTagName("Customers");
+    var customerList = document.getElementById("customers");
+    for (var customerIndex = 0; customerIndex < customers.length; customerIndex++) {
+        setTimeout(function (customerIndex) {
+            var customerNode = customers[customerIndex];
+            var contactNameAttribute = customerNode.getAttribute("ContactName");
+            var contactNameElement = document.createElement("li");
+            contactNameElement.innerHTML = contactNameAttribute;
+            var ordersList = document.createElement("ul");
+            var orders = customerNode.getElementsByTagName("Orders");
+            for (var orderIndex = 0; orderIndex < orders.length; orderIndex++) {
+                var orderNode = orders[orderIndex];
+                var orderID = orderNode.getAttribute("OrderID")
+                var orderDate = new Date(Date.parse(orderNode.getAttribute("OrderDate")))
+                var orderElement = document.createElement("li")
+                orderElement.innerHTML = orderID + " - " + orderDate.toLocaleDateString("en-US")
+                ordersList.appendChild(orderElement)
+                var orderDetailList = document.createElement("ul")
+                var orderDetails = orderNode.getElementsByTagName("Order_x0020_Details");
+                for (var orderDetailIndex = 0; orderDetailIndex < orderDetails.length; orderDetailIndex++) {
+                    var orderDetail = orderDetails[orderDetailIndex];
+                    var product = orderDetail.getElementsByTagName("Products").item(0).getAttribute("ProductName");
+                    var unitPrice = parseFloat(orderDetail.getAttribute("UnitPrice"));
+                    var quantity = parseInt(orderDetail.getAttribute("Quantity"));
+                    var orderDetailElement = document.createElement("li");
+                    orderDetailElement.innerHTML = product + " (" + unitPrice.toFixed(2) + " × " + quantity + ") = " + (unitPrice * quantity).toFixed(2)
+                    orderDetailList.appendChild(orderDetailElement);
+                }
+                ordersList.appendChild(orderDetailList);
+            }
+            contactNameElement.appendChild(ordersList);
+            customerList.appendChild(contactNameElement);
+        }, 1000, customerIndex);
+    }
+}
+loadDoc();
