0d31cdf3f512a1685b9fc562d0e1edc4a06c0dca
kevinsmgov <kevin.hobson@smgov.net>
2018-05-27 14:19:53 -0700

    add json variation

---
+++ asp_classic/App_Data/client-sql-xml-json.xslt
@@ -0,0 +1,27 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0"
+                xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="text" indent="no"/>
+  
+  <xsl:template match="Products"><xsl:value-of select="@ProductName"/></xsl:template>
+  
+  <xsl:template match="Order_x0020_Details">{ <xsl:if test="./Products">
+    "productName": "<xsl:apply-templates select="Products"/>", </xsl:if>
+    "unitPrice": <xsl:value-of select="number(@UnitPrice)"/>, 
+    "quantity": <xsl:value-of select="number(@Quantity)"/> 
+    }<xsl:if test="count(following-sibling::*) &gt; 0">, </xsl:if></xsl:template>
+
+  <xsl:template match="Orders">{ 
+  "orderID": "<xsl:value-of select="@OrderID"/>", 
+  "orderDate": "<xsl:value-of select="@OrderDate"/>", 
+  "orderDetails": [<xsl:apply-templates/>]}<xsl:if test="count(following-sibling::*) &gt; 0">, </xsl:if></xsl:template>
+
+  <xsl:template match="Customers">{
+  "contactName": "<xsl:value-of select="@ContactName"/>", 
+  "orders": [<xsl:apply-templates/>]}<xsl:if test="count(following-sibling::*) &gt; 0">, </xsl:if></xsl:template>
+
+  <xsl:template match="root">[
+    <xsl:apply-templates/>
+    ]</xsl:template>
+
+</xsl:stylesheet>
---
+++ asp_classic/client-sql-xml-json.js
@@ -0,0 +1,48 @@
+﻿
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            myFunction(this);
+        }
+    };
+    xhttp.open("GET", "client-sql-xml-json.asp?json", true);
+    xhttp.send();
+}
+function myFunction(xhr) {
+    var customers = JSON.parse(xhr.responseText);
+    var customerList = document.getElementById("customers");
+    for (var customerIndex = 0; customerIndex < customers.length; customerIndex++) {
+        setTimeout(function (customerIndex) {
+            var customerNode = customers[customerIndex];
+            var contactNameAttribute = customerNode.contactName;
+            var contactNameElement = document.createElement("li");
+            contactNameElement.innerHTML = contactNameAttribute;
+            var ordersList = document.createElement("ul");
+            var orders = customerNode.orders;
+            for (var orderIndex = 0; orderIndex < orders.length; orderIndex++) {
+                var orderNode = orders[orderIndex];
+                var orderID = orderNode.orderID;
+                var orderDate = new Date(Date.parse(orderNode.orderDate));
+                var orderElement = document.createElement("li");
+                orderElement.innerHTML = orderID + " - " + orderDate.toLocaleDateString("en-US");
+                ordersList.appendChild(orderElement);
+                var orderDetailList = document.createElement("ul");
+                var orderDetails = orderNode.orderDetails;
+                for (var orderDetailIndex = 0; orderDetailIndex < orderDetails.length; orderDetailIndex++) {
+                    var orderDetail = orderDetails[orderDetailIndex];
+                    var product = orderDetail.productName;
+                    var unitPrice = parseFloat(orderDetail.unitPrice);
+                    var quantity = parseInt(orderDetail.quantity);
+                    var orderDetailElement = document.createElement("li");
+                    orderDetailElement.innerHTML = product + " (" + unitPrice.toFixed(2) + " × " + quantity + ") = " + (unitPrice * quantity).toFixed(2);
+                    orderDetailList.appendChild(orderDetailElement);
+                }
+                ordersList.appendChild(orderDetailList);
+            }
+            contactNameElement.appendChild(ordersList);
+            customerList.appendChild(contactNameElement);
+        }, 1000, customerIndex);
+    }
+}
+loadDoc();
