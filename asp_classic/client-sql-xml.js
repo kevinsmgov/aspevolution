2f912709476aad2076f6d5e7b43ff0cc5a55c098
kevinsmgov <kevin.hobson@smgov.net>
2018-05-28 14:54:52 -0700

    clean up code

--- asp_classic/client-sql-xml.js
+++ asp_classic/client-sql-xml.js
@@ -2,7 +2,7 @@
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             myFunction(this);
         }
     };
@@ -23,11 +23,11 @@ function myFunction(xhr) {
             var orders = customerNode.getElementsByTagName("Orders");
             for (var orderIndex = 0; orderIndex < orders.length; orderIndex++) {
                 var orderNode = orders[orderIndex];
-                var orderID = orderNode.getAttribute("OrderID")
-                var orderDate = new Date(Date.parse(orderNode.getAttribute("OrderDate")))
-                var orderLI = document.createElement("li")
-                orderLI.innerHTML = orderID + " - " + orderDate.toLocaleDateString("en-US")
-                var orderDetailsUL = document.createElement("ul")
+                var orderID = orderNode.getAttribute("OrderID");
+                var orderDate = new Date(Date.parse(orderNode.getAttribute("OrderDate")));
+                var orderLI = document.createElement("li");
+                orderLI.innerHTML = orderID + " - " + orderDate.toLocaleDateString("en-US");
+                var orderDetailsUL = document.createElement("ul");
                 var orderDetails = orderNode.getElementsByTagName("Order_x0020_Details");
                 for (var orderDetailIndex = 0; orderDetailIndex < orderDetails.length; orderDetailIndex++) {
                     var orderDetail = orderDetails[orderDetailIndex];
@@ -35,11 +35,11 @@ function myFunction(xhr) {
                     var unitPrice = parseFloat(orderDetail.getAttribute("UnitPrice"));
                     var quantity = parseInt(orderDetail.getAttribute("Quantity"));
                     var orderDetailLI = document.createElement("li");
-                    orderDetailLI.innerHTML = product + " (" + unitPrice.toFixed(2) + " × " + quantity + ") = " + (unitPrice * quantity).toFixed(2)
+                    orderDetailLI.innerHTML = product + " (" + unitPrice.toFixed(2) + " × " + quantity + ") = " + (unitPrice * quantity).toFixed(2);
                     orderDetailsUL.appendChild(orderDetailLI);
                 }
                 orderLI.appendChild(orderDetailsUL);
-                ordersUL.appendChild(orderLI)
+                ordersUL.appendChild(orderLI);
             }
             customerLI.appendChild(ordersUL);
             customersUL.appendChild(customerLI);
